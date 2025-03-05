8b2075f6200ee2c4357fa2bef09184c2a1ae4bd8
Vincenzo Baia <vincenzo@vincenzos-mbp.fairfield.edu>
2025-03-05 15:04:09 -0500

    contact final

--- template/Contact.java
+++ template/Contact.java
@@ -1,5 +1,7 @@
 package template;
 
+import java.util.*;
+
 /**
  * model a Contact 
  * Contact has a name & list of phonebook entries
@@ -12,17 +14,46 @@ package template;
 
 class Contact {
     private String name;
+    private List<PhonebookEntry> phonebookEntries;
 
     public Contact (String name) {
         this.name = name;
+        this.phonebookEntries = new ArrayList<>();
     }
 
     public void addPhonebookEntry(PhonebookEntry phonebookEntry) {
         this.phonebookEntries.add(phonebookEntry);
     }
 
+    public void addPhonebookEntries(String phoneNumber, String type) {
+        PhonebookEntry entry = new PhonebookEntry(phoneNumber, type);
+        this.phonebookEntries.add(entry);
+    }
+
     public List<PhonebookEntry> getPhonebookEntries() {
         return phonebookEntries;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false; 
+        if (getClass() != obj.getClass()) return false;
+        Contact other = (Contact) obj;
+        return Objects.equals(name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        if (name == null) {
+            return 0;
+        } else {
+            return name.hashCode();
+        }
+    }
+        
 }
